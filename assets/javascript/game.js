76f854bcab5e6d8dc12f51c6e8cca7ec2b0ffb77
James McMillan <jmcmillan29@gmail.com>
2017-10-23 20:34:37 -0700

    built the phrase array and started to pull the phrases

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,8 +2,24 @@
 
 // On Page Load
 
-window.onload = () => {
+window.onload = function() {
 
-	
+	var userGuesses = [];
+	var usedPhrases = [];
+	var thePhrase = [];
+	var counter;
+	var spaces;
+
+	var phraseArray = [ 'first', 'second', 'third', 'fourth', 'fifth'];
+
+
+	var getRandomPhrase = function() {
+
+		thePhrase = phraseArray[Math.floor(Math.random() * phraseArray.length)]
+
+		console.log(thePhrase);
+	}
+
+	getRandomPhrase();
 
 };
